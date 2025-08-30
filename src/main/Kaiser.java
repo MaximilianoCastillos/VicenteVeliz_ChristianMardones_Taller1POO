d862cc6a6c34e1a1d18b7c36a877db1ab75f8399
Chente <Chente@DESKTOP-P0186SE>
2025-08-30 16:25:47 -0400

    PRUEBA

--- src/main/Kaiser.java
+++ src/main/Kaiser.java
@@ -6,8 +6,9 @@ public class Kaiser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Chente x JoAN");
-		
 		//PARTE DE ADMIN ESPECIFICA
+		//PRUEBA
+		
 		
 	}
 
