64e7e4680e4e72b6723ee4603c34af13459a7763
Chente <Chente@DESKTOP-P0186SE>
2025-08-30 16:12:56 -0400

    HOLA

--- src/main/Kaiser.java
+++ src/main/Kaiser.java
@@ -1,10 +1,16 @@
 package main;
 
+//PARTE DE ADMIN
 public class Kaiser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Chente x JoAN");
+		
+		//PARTE DE ADMIN ESPECIFICA
+		
 	}
 
 }
+
+//PARTE TERMINADA
