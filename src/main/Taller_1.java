d963a6645a4e5ac2f92e2ad3f07e3f89220dc3c2
Chente <Chente@DESKTOP-P0186SE>
2025-08-31 14:41:56 -0400

    Agregado experimentos.txt al codigo

--- src/main/Taller_1.java
+++ src/main/Taller_1.java
@@ -1,9 +1,15 @@
 package main;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Taller_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		File experimentos = new File("archivos/experimentos.txt");
+		Scanner sc = new Scanner(experimentos);
+		
 
 	}
 
