041ca3ea1dcee16af1fbf61f0018bc2978e9168b
Admn <Admn@DESKTOP-12NSEQS>
2026-03-31 22:01:43 -0300

    hice el menu pero le falta control de error y el menu de usuario por completar, lei los archivos los separe en varias listas y respondi del menu de analisis la 1 y 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,118 +1,208 @@
 package main;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException {
+		File arch = new File("archivos/usuarios.txt");
+		File arch2 = new File("archivos/registros.txt");
+		
+		Scanner scan = new Scanner(arch);
+		Scanner scan2 = new Scanner(arch2);
 
-        Repositorio repo = Repositorio.getInstance();
-        repo.cargarUsuarios();
-        repo.cargarProyectos();
-        repo.cargarTareas();
+		
+		String[] usuario = new String[300];
+		String[] contras = new String[300];
+		
+		
+		String[] ides = new String[300];
+		String[] fechas = new String[300];
+		String[] actividades = new String[300];
+		int[] horas = new int[300];
+		
+		int[] contador = new int[300]; 
 
-        Usuario revisar = null;
-        Scanner scan = new Scanner(System.in);
-        Scanner eleccion = new Scanner(System.in);
+		
+		int j = 0;
+		int i = 0;
+		
+		
+		try {
+			while (scan.hasNextLine()) {
+				String linea = scan.nextLine();
+				String[] partes = linea.split(";");
+				String nombre = partes[0];
+				String contraseña = partes[1]; 
+				
+				usuario[j] = nombre;
+				contras[j] = contraseña;
+				
+				j++;
+				
+				
+				
+			}
+			
+			while (scan2.hasNextLine()) {
+			    String linea2 = scan2.nextLine();
+			    String[] partes2 = linea2.split(";"); 
+			    String id = partes2[0];
+			    String fecha = partes2[1];
+			    
+			    int hora = Integer.parseInt(partes2[2]);
+			    String actividad = partes2[3];
+			    boolean confirmar = false;
+			    
+			   
+			    
+			    for (int l = 0; l < i; l++) {
+			        if (actividad.equals(actividades[l])) {
+			            confirmar = true;
+			            contador[l] += 1;
+			            break;
+			        }
+			    }
 
-        boolean seguirPrograma = true;
+			    if (!confirmar) {
+			        ides[i] = id; 
+			        fechas[i] = fecha;
+			        actividades[i] = actividad;
+			        horas[i] = hora;  
+			        contador[i] = 1;
+			        i++;
+			    }
+			}
+			
+			
+		} catch (Exception e) {
 
-        while (seguirPrograma)
-        /** mantiene activa el programa hasta que se desee salir */{
-            while (revisar == null) {
-                revisar = Funciones.login();
-            }
-            if (revisar.getRol().equalsIgnoreCase("Administrador"))
-            /** en caso que el usuario sea administrador entra por aca */ {
-                Administrador admin = (Administrador) revisar;
+		}
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		boolean seguirViendo = true;
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		while (seguirViendo) {
+			
+				try {
+					System.out.println("1) Menu de Usuarios");
+					System.out.println("2) Menu de Analisis");
+					System.out.println("3) Salir");
+					System.out.println("ingrese opcion");
+					
+					int opcionUsuario = teclado.nextInt();
+					
+					switch (opcionUsuario) {
+					
+					
+						case 1:
+							System.out.println("menu usuario");
+							
+							
+							
+							
+							
+							
+							
+						case 2:
+							System.out.println("menu analisis");
+							System.out.println("-----------");
+							
+							System.out.println("1) Actividad más realizada");
+							System.out.println("2) Actividad más realizada por cada usuario");
+							System.out.println("3) Usuario con mayor procastinacion");
+							System.out.println("4) Ver todas las actividades");
+							System.out.println("5) Salir");
+							
+							int analisis = teclado.nextInt();
+							
+							switch (analisis) {
+								case 1:
+									String mayor = "";
+									int maximo = 0;
+									
+									for (int v = 0; v < i ; v++) {
+										if (contador[v] > maximo) {
+											mayor = actividades[v];
+											maximo = contador[v];
+										}
+									
+										
+										
+									
+									}
+									System.out.println("el mayor es " + mayor);
+									System.out.println(" ");
+									
+								case 2:
+								    
+								    for (int d = 0; d < j; d++) {  
+								        String persona = usuario[d];  
+								        int maximoHoras = 0;          
+								        String actividadMax = "";     
+								        
+								       
+								        for (int r = 0; r < i; r++) {  
+								            if (ides[r].equalsIgnoreCase(persona)) {  
+								                if (horas[r] > maximoHoras) { 
+								                    maximoHoras = horas[r];  
+								                    actividadMax = actividades[r];  
+								                }
+								            }
+								        }
+								        
+								       
+								        System.out.println( persona +  " "+  actividadMax + " con " + maximoHoras + " horas.");
+								    }
+								    break;
+									
+									
+									
+									
 
-                admin.menuAdmin();
-                /** muestra el menu constantemente */
-                int opcion = 0;
+									
+							
+							
+							}
+							
+							
+							
 
-                System.out.print("Elija una opcion: ");
-                
-                try {
-                    opcion = eleccion.nextInt(); // Intenta leer una opción numérica
+							
+					}
+					
+					
+				} catch (Exception e) {
+					System.out.println("aweoanao");
+				}
+				
 
-                    switch (opcion)
-                    /** vee que opcion se eligio y revisa que este dentro de los limites */ {
-                        case 1:
-                            admin.listaProyectoTareas();
-                            break;
-                        case 2:
-                            admin.gestionerProyectos();
-                            break;
-                        case 3:
-                            admin.gestionarTareas();
-                            break;
-                        case 4:
-                            admin.asignarPrioridades();
-                            break;
-                        case 5:
-                            admin.generarReporte();
-                            break;
-                        case 6:
-                            revisar = null;
-                            break;
 
-                        default:
-                            System.out.println(" ");
-                            System.out.println("------------");
-                            System.out.println("Opcion invalida");
-                            System.out.println("------------");
-                            System.out.println(" ");
-                    }
-                } catch (IllegalArgumentException e) {
-                    
-                    System.out.println("Entrada inválida, por favor ingrese un número.");
-                    eleccion.nextLine(); 
-                }
-            }
-            if (revisar.getRol().equalsIgnoreCase("Colaborador"))
-            /** entra aca en caso que el usuario sea colaborador */ {
-                Colaborador cola = (Colaborador) revisar;
-                cola.menuColaborador();
-                int opcion = 0;
+				
+				
+				
+				
+				
+				
+		
+			 
+		}
+		
+		
+		
+		
+		
+	}
 
-                try {
-                    opcion = eleccion.nextInt(); // Intenta leer una opción numérica
-
-                    switch (opcion)
-                    /** vee que opcion se eligio y revisa que este dentro de los limites */ {
-                        case 1:
-                            cola.verTareas();
-                            break;
-
-                        case 2:
-                            cola.tareasProyectos();
-                            break;
-
-                        case 3:
-                            cola.cambiarEstado();
-                            break;
-                        case 4:
-                            cola.aplicarVisitor();
-                            break;
-
-                        case 5:
-                            revisar = null;
-                            break;
-
-                        default:
-                            System.out.println(" ");
-                            System.out.println("------------");
-                            System.out.println("Opcion invalida");
-                            System.out.println("------------");
-                            System.out.println(" ");
-                    }
-                } catch (InputMismatchException e) {
-                    System.out.println("Entrada inválida, por favor ingrese un número.");
-                    eleccion.nextLine(); 
-                }
-            }
-        }
-    }
 }
